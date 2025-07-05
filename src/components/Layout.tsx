56fcc74f07211e0c375bf176ccaaa9aeff5837c8
Seven <274852778@qq.com>
2025-07-05 14:54:32 +0800

    feat: update style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
 import styles from './Layout.module.css';
@@ -8,9 +9,26 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+  
+  // Define routes that should hide the navbar
+  const hideNavbarRoutes = [
+    '/blogs/[id]',
+    '/events/[id]',
+    '/blogs/[id]/edit',
+    '/events/[id]/edit'
+  ];
+  
+  // Check if current route should hide navbar
+  const shouldHideNavbar = hideNavbarRoutes.some(route => {
+    // Convert route pattern to regex
+    const routeRegex = new RegExp('^' + route.replace(/\[.*?\]/g, '[^/]+') + '$');
+    return routeRegex.test(router.pathname);
+  });
+
   return (
     <div className={styles.layout}>
-      <Header />
+      {!shouldHideNavbar && <Header />}
       <main className={styles.main}>{children}</main>
       <Footer />
     </div>
